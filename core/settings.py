a8f6733742ed640b23e13b59aaa34cfe0641f1e1
Asilbek <asilbek.sadullayev000@example.com>
2025-09-09 11:02:29 +0500

    core static

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","35.72.2.191"]
 
 
 # Application definition
