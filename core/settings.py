382f84151b7167e068255985e5968020b0d383ea
Asilbek <asilbek.sadullayev000@example.com>
2025-09-09 10:33:11 +0500

    core static

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [BASE_DIR / 'core/static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
